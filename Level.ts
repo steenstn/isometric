196926256d09f9eafe1937dc16a5ef4413f17920
steenstn <alest849@gmail.com>
2018-10-14 12:52:49 +0200

    Some cleanup

--- Level.ts
+++ Level.ts
@@ -11,11 +11,10 @@ class Level {
     }
 
     static render(context : any) {
-        let tileSize = 32;
         for(var i = 0; i < Level.levelWidth; i++) {
             for(var j = 0; j < Level.levelWidth; j++) {
       
-              var p = new Point(i*tileSize, j*tileSize);
+              var p = new Point(i*Level.tileSize, j*Level.tileSize);
               
               context.fillStyle = Level.currentLevel[i+j*Level.levelWidth] === 0 ? "#5af15a" : "#a0a0a0";
               if(Level.currentLevel[i+j*Level.levelWidth] === 3) {
@@ -23,10 +22,10 @@ class Level {
               }
             
             var pIso = Coordinates.toIso(p);
-            context.drawImage(Level.tileset,Level.currentLevel[i+j*Level.levelWidth]*64,0, tileSize*2,
-            tileSize, Viewport.isoPos().x+pIso.x-tileSize,Viewport.isoPos().y+pIso.y, tileSize*2, tileSize);
+            context.drawImage(Level.tileset,Level.currentLevel[i+j*Level.levelWidth]*64,0, Level.tileSize*2,
+                Level.tileSize, Viewport.isoPos().x+pIso.x-Level.tileSize,Viewport.isoPos().y+pIso.y, Level.tileSize*2, Level.tileSize);
             
             }
-          }
+        }
     }
 }
--- js/Level.js
+++ js/Level.js
@@ -6,16 +6,15 @@ var Level = (function () {
         Level.tileset.src = "sd.png";
     };
     Level.render = function (context) {
-        var tileSize = 32;
         for (var i = 0; i < Level.levelWidth; i++) {
             for (var j = 0; j < Level.levelWidth; j++) {
-                var p = new Point(i * tileSize, j * tileSize);
+                var p = new Point(i * Level.tileSize, j * Level.tileSize);
                 context.fillStyle = Level.currentLevel[i + j * Level.levelWidth] === 0 ? "#5af15a" : "#a0a0a0";
                 if (Level.currentLevel[i + j * Level.levelWidth] === 3) {
                     context.fillStyle = "#f00";
                 }
                 var pIso = Coordinates.toIso(p);
-                context.drawImage(Level.tileset, Level.currentLevel[i + j * Level.levelWidth] * 64, 0, tileSize * 2, tileSize, Viewport.isoPos().x + pIso.x - tileSize, Viewport.isoPos().y + pIso.y, tileSize * 2, tileSize);
+                context.drawImage(Level.tileset, Level.currentLevel[i + j * Level.levelWidth] * 64, 0, Level.tileSize * 2, Level.tileSize, Viewport.isoPos().x + pIso.x - Level.tileSize, Viewport.isoPos().y + pIso.y, Level.tileSize * 2, Level.tileSize);
             }
         }
     };
