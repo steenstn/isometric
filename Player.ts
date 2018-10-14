59daa56ae263d510ec87478ddef82ee80241b292
steenstn <alest849@gmail.com>
2018-10-14 12:15:36 +0200

    Simple collision

--- Player.ts
+++ Player.ts
@@ -13,10 +13,10 @@ class Player {
     }
 
     moveTowardsGoal() {
-        this.oldPos = this.pos;
         if(Math.abs(this.pos.x - this.goalPos.x) < 5 && Math.abs(this.pos.y - this.goalPos.y) < 5) {
             return;
         }
+        this.oldPos = new Point(this.pos.x, this.pos.y);
         
         if(this.goalPos.x > this.pos.x) {
             this.pos.x++;
@@ -29,15 +29,23 @@ class Player {
             this.pos.y--;
         }
         
+        let levelPos = Coordinates.getTileCoordinates(this.pos,32);
+        if(Level.currentLevel[levelPos.x + levelPos.y*Level.levelWidth]!=0) {
+            
+            this.pos = this.oldPos;
+        }
     }
 
     render(context : any) {
-        context.fillStyle = "#33f";
+        context.fillStyle = "#11f";
         let isoPos = Coordinates.toIso(this.pos);
         context.fillRect(Viewport.isoPos().x + isoPos.x, Viewport.isoPos().y + isoPos.y, 10, 16);
         context.fillStyle = "#f00";
         let isoGoalPos = Coordinates.toIso(this.goalPos);
         context.fillRect(Viewport.isoPos().x + isoGoalPos.x, Viewport.isoPos().y + isoGoalPos.y, 5,5);
+        let levelPos = Coordinates.getTileCoordinates(this.pos,32);
+        
+        context.fillText(Level.currentLevel[levelPos.x + levelPos.y*Level.levelWidth], 10, 10);
         
     }
 }
--- js/Player.js
+++ js/Player.js
@@ -8,10 +8,10 @@ var Player = (function () {
         this.goalPos = goal;
     };
     Player.prototype.moveTowardsGoal = function () {
-        this.oldPos = this.pos;
         if (Math.abs(this.pos.x - this.goalPos.x) < 5 && Math.abs(this.pos.y - this.goalPos.y) < 5) {
             return;
         }
+        this.oldPos = new Point(this.pos.x, this.pos.y);
         if (this.goalPos.x > this.pos.x) {
             this.pos.x++;
         }
@@ -24,14 +24,20 @@ var Player = (function () {
         else {
             this.pos.y--;
         }
+        var levelPos = Coordinates.getTileCoordinates(this.pos, 32);
+        if (Level.currentLevel[levelPos.x + levelPos.y * Level.levelWidth] != 0) {
+            this.pos = this.oldPos;
+        }
     };
     Player.prototype.render = function (context) {
-        context.fillStyle = "#33f";
+        context.fillStyle = "#11f";
         var isoPos = Coordinates.toIso(this.pos);
         context.fillRect(Viewport.isoPos().x + isoPos.x, Viewport.isoPos().y + isoPos.y, 10, 16);
         context.fillStyle = "#f00";
         var isoGoalPos = Coordinates.toIso(this.goalPos);
         context.fillRect(Viewport.isoPos().x + isoGoalPos.x, Viewport.isoPos().y + isoGoalPos.y, 5, 5);
+        var levelPos = Coordinates.getTileCoordinates(this.pos, 32);
+        context.fillText(Level.currentLevel[levelPos.x + levelPos.y * Level.levelWidth], 10, 10);
     };
     return Player;
 }());
