fb95d3664b7fb3e6e9f1712617abb24cfc7170eb
steenstn <alest849@gmail.com>
2018-12-20 16:00:49 +0100

    Added keycode class

---
+++ Keycode.ts
@@ -0,0 +1,7 @@
+class Keycode {
+    static readonly A = 65;
+    static readonly D = 68;
+    static readonly S = 83;
+    static readonly O = 79;
+    static readonly W = 87;
+}
---
+++ js/Keycode.js
@@ -0,0 +1,7 @@
+class Keycode {
+}
+Keycode.A = 65;
+Keycode.D = 68;
+Keycode.S = 83;
+Keycode.O = 79;
+Keycode.W = 87;
